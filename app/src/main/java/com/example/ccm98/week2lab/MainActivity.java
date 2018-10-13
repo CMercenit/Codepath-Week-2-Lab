8901856381db0fb1bc051c78317545d638eff178
CMercenit <ccm98@yahoo.com>
2018-10-13 17:18:31 -0400

    Completed required tasks.

--- app/src/main/java/com/example/ccm98/week2lab/MainActivity.java
+++ app/src/main/java/com/example/ccm98/week2lab/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.ccm98.week2lab;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcardQuestion).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                findViewById(R.id.flashcardAnswer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcardQuestion).setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
