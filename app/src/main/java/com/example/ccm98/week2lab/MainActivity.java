2b38783cb5dc92e5640cd7dea123b692437403c4
CMercenit <ccm98@yahoo.com>
2018-11-16 23:11:58 -0500

    Lab 3 complete.

--- app/src/main/java/com/example/ccm98/week2lab/MainActivity.java
+++ app/src/main/java/com/example/ccm98/week2lab/MainActivity.java
@@ -6,14 +6,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
 {
+    private FlashcardDatabase flashDatabase;
+    private List<Flashcard> myFlashcards;
+    private int myCurrentCard = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flashDatabase = new FlashcardDatabase(getApplicationContext());
+        myFlashcards = flashDatabase.getAllCards();
+
+        if (myFlashcards != null && myFlashcards.size() > 0)
+        {
+            ((TextView) findViewById(R.id.flashcardQuestion)).setText(myFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcardAnswer)).setText(myFlashcards.get(0).getAnswer());
+        }
+        else
+        {
+            ((TextView) findViewById(R.id.flashcardQuestion)).setText("Add a question!");
+            ((TextView) findViewById(R.id.flashcardAnswer)).setText("Add a question!");
+        }
 
         findViewById(R.id.flashcardQuestion).setOnClickListener(new View.OnClickListener()
         {
@@ -54,6 +72,57 @@ public class MainActivity extends AppCompatActivity
                 MainActivity.this.startActivityForResult(intent, 2);
             }
         });
+
+        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(myFlashcards.size() > 0)
+                {
+                    myCurrentCard++;
+
+                    if (myCurrentCard > (myFlashcards.size() - 1)) {
+                        myCurrentCard = 0;
+                    }
+
+                    ((TextView) findViewById(R.id.flashcardQuestion)).setText(myFlashcards.get(myCurrentCard).getQuestion());
+                    ((TextView) findViewById(R.id.flashcardAnswer)).setText(myFlashcards.get(myCurrentCard).getAnswer());
+
+                    if (findViewById(R.id.flashcardAnswer).getVisibility() == View.VISIBLE) {
+                        findViewById(R.id.flashcardAnswer).setVisibility(View.INVISIBLE);
+                        findViewById(R.id.flashcardQuestion).setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+        findViewById(R.id.deleteDataButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashDatabase.deleteCard(((TextView) findViewById(R.id.flashcardQuestion)).getText().toString());
+                //System.out.println(((TextView) findViewById(R.id.flashcardQuestion)).getText().toString());
+                myFlashcards = flashDatabase.getAllCards();
+                if (myFlashcards == null || myFlashcards.size() == 0)
+                {
+                    ((TextView) findViewById(R.id.flashcardQuestion)).setText("Add a question!");
+                    ((TextView) findViewById(R.id.flashcardAnswer)).setText("Add a question!");
+                }
+                else
+                {
+                    if(myCurrentCard > 0)
+                    {
+                        myCurrentCard--;
+                    }
+                    ((TextView) findViewById(R.id.flashcardQuestion)).setText(myFlashcards.get(myCurrentCard).getQuestion());
+                    ((TextView) findViewById(R.id.flashcardAnswer)).setText(myFlashcards.get(myCurrentCard).getAnswer());
+
+                    if(findViewById(R.id.flashcardAnswer).getVisibility() == View.VISIBLE)
+                    {
+                        findViewById(R.id.flashcardAnswer).setVisibility(View.INVISIBLE);
+                        findViewById(R.id.flashcardQuestion).setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -68,6 +137,10 @@ public class MainActivity extends AppCompatActivity
 
             ((TextView)findViewById(R.id.flashcardQuestion)).setText(string1);
             ((TextView)findViewById(R.id.flashcardAnswer)).setText(string2);
+
+            flashDatabase.insertCard(new Flashcard(string1, string2));
+            myFlashcards = flashDatabase.getAllCards();
+            myCurrentCard++;
         }
     }
 }
